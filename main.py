46dc7f5565277038f882ff649be4fa7009cf31d4
spatel051 <77207491+spatel051@users.noreply.github.com>
2021-12-18 12:52:32 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, jsonify, request
+import csv
+
+all_movies = []
+with open("movies.csv", encoding = "utf8") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_movies = data[1:]
+
+liked_movies = []
+not_liked_movies = []
+did_not_watch = []
+
+app = Flask(__name__)
+
+@app.route('/get-movies')
+def get_movie():
+    return jsonify({
+        'data': all_movies[0],
+        'status': 'success'
+    })
+
+@app.route('/like-movie', methods = ['POST'])
+def liked_movie():
+    all_movies = []
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    liked_movies.append(movie)
+    return jsonify({
+        'status': 'success'
+    }), 201
+
+@app.route('/unlike-movie', methods = ['POST'])
+def unliked_movie():
+    all_movies = []
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    not_liked_movies.append(movie)
+    return jsonify({
+        'status': 'success'
+    }), 201
+
+@app.route('/did-not-watch', methods = ['POST'])
+def did_not_watch():
+    all_movies = []
+    movie = all_movies[0]
+    all_movies = all_movies[1:]
+    did_not_watch.append(movie)
+    return jsonify({
+        'status': 'success'
+    }), 201
+
+if __name__ == "__main__":
+    app.run()
