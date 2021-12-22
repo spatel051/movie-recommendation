8cb8881c0a98046e9c35adc738b191fd690efe17
spatel051 <77207491+spatel051@users.noreply.github.com>
2021-12-22 15:02:29 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,96 @@
 from flask import Flask, jsonify, request
-import csv
 
-all_movies = []
-with open("movies.csv", encoding = "utf8") as f:
-    reader = csv.reader(f)
-    data = list(reader)
-    all_movies = data[1:]
-
-liked_movies = []
-not_liked_movies = []
-did_not_watch = []
+from storage import all_movies, liked_movies, not_liked_movies, did_not_watch
+from demographic_filtering import output
+from content_filtering import get_recommendations
 
 app = Flask(__name__)
 
-@app.route('/get-movies')
+@app.route("/get-movie")
 def get_movie():
+    movie_data = {
+        "title": all_movies[0][19],
+        "poster_link": all_movies[0][27],
+        "release_date": all_movies[0][13] or "N/A",
+        "duration": all_movies[0][15],
+        "rating": all_movies[0][20],
+        "overview": all_movies[0][9]
+    }
     return jsonify({
-        'data': all_movies[0],
-        'status': 'success'
+        "data": movie_data,
+        "status": "success"
     })
 
-@app.route('/like-movie', methods = ['POST'])
+@app.route("/liked-movie", methods=["POST"])
 def liked_movie():
-    all_movies = []
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     liked_movies.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        'status': 'success'
+        "status": "success"
     }), 201
 
-@app.route('/unlike-movie', methods = ['POST'])
+@app.route("/unliked-movie", methods=["POST"])
 def unliked_movie():
-    all_movies = []
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     not_liked_movies.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        'status': 'success'
+        "status": "success"
     }), 201
 
-@app.route('/did-not-watch', methods = ['POST'])
-def did_not_watch():
-    all_movies = []
+@app.route("/did-not-watch", methods=["POST"])
+def did_not_watch_view():
     movie = all_movies[0]
-    all_movies = all_movies[1:]
     did_not_watch.append(movie)
+    all_movies.pop(0)
     return jsonify({
-        'status': 'success'
+        "status": "success"
     }), 201
+
+@app.route("/popular-movies")
+def popular_movies():
+    movie_data = []
+    for movie in output:
+        _d = {
+            "title": movie[0],
+            "poster_link": movie[1],
+            "release_date": movie[2] or "N/A",
+            "duration": movie[3],
+            "rating": movie[4],
+            "overview": movie[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
+
+@app.route("/recommended-movies")
+def recommended_movies():
+    all_recommended = []
+    for liked_movie in liked_movies:
+        output = get_recommendations(liked_movie[19])
+        for data in output:
+            all_recommended.append(data)
+    import itertools
+    all_recommended.sort()
+    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
+    movie_data = []
+    for recommended in all_recommended:
+        _d = {
+            "title": recommended[0],
+            "poster_link": recommended[1],
+            "release_date": recommended[2] or "N/A",
+            "duration": recommended[3],
+            "rating": recommended[4],
+            "overview": recommended[5]
+        }
+        movie_data.append(_d)
+    return jsonify({
+        "data": movie_data,
+        "status": "success"
+    }), 200
 
 if __name__ == "__main__":
-    app.run()
+  app.run()
